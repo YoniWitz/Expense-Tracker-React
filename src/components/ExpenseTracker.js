c5a561f6616e1593355ba1e0b383477f4727686e
Yoni Hirshowitz <jonhirsh39@gmail.com>
2020-01-03 13:26:51 -0500

    added two components

--- src/components/ExpenseTracker.js
+++ src/components/ExpenseTracker.js
@@ -1,34 +1,20 @@
 import React from 'react'
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
+
+
+import FormComponent from './FormComponent'
+import TableComponent from './TableComponent'
 
 
 class ExpenseTracker extends React.Component {
-    constructor(){
+    constructor() {
         super();
     }
 
-    render(){
-    return (<Form>
-        <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-      </Form.Text>
-        </Form.Group>
-
-        <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-            Submit
-    </Button>
-    </Form>)
+    render() {
+        return (
+            <div><FormComponent />
+                <TableComponent />
+            </div>)
     }
 }
 export default ExpenseTracker
