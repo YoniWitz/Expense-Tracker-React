ee15bee6313ee604cc8282ff5b2407dc09f34fcd
Yoni Hirshowitz <jonhirsh39@gmail.com>
2020-01-03 14:00:05 -0500

    completed table'

--- src/components/ExpenseTracker.js
+++ src/components/ExpenseTracker.js
@@ -8,12 +8,28 @@ import TableComponent from './TableComponent'
 class ExpenseTracker extends React.Component {
     constructor() {
         super();
+        this.state = {
+            description: "",
+            date: "",
+            amount: "",
+            where: "",
+            expenses:[]
+        }
+        this.changeHandle = this.changeHandle.bind(this);
+    }
+
+    changeHandle(event) {
+        let { name, value } = event.target;
+        this.setState({
+            [name]: value
+        });
     }
 
     render() {
         return (
-            <div><FormComponent />
-                <TableComponent />
+            <div>
+                <FormComponent state={this.state} changeHandle={this.changeHandle} />
+                <TableComponent state={this.state}/>
             </div>)
     }
 }
--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -3,26 +3,34 @@ import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
 function FormComponent(props) {
-    return (<Form>
-        <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-      </Form.Text>
-        </Form.Group>
+    let state = props.state;
+    let changeHandle = props.changeHandle;
+    //console.log(state);
+    return (
+        <Form>
+            <Form.Group controlId="formBasicDate">
+                <Form.Label>Date:</Form.Label>
+                <Form.Control value={state.date} name="date" onChange={changeHandle} type="date" placeholder="mm/dd/yyyy" />            
+            </Form.Group>
 
-        <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-            Submit
-    </Button>
-    </Form>)
+            <Form.Group controlId="formBasicDescription">
+                <Form.Label>Description:</Form.Label>
+                <Form.Control value={state.description} name="description" onChange={changeHandle} type="text" placeholder="What did you spend on?" />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicAmount">
+                <Form.Label>Amount:</Form.Label>
+                <Form.Control value={state.amount} name="amount" onChange={changeHandle} type="number" placeholder="How much?" />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicWhere">
+                <Form.Label>Where:</Form.Label>
+                <Form.Control value={state.where} name="where" onChange={changeHandle} type="text" placeholder="Ebay, Moments Cafe, etc." />
+            </Form.Group>
+           
+            <Button variant="primary" type="submit">Submit</Button>
+        </Form>
+    )
 }
 
 export default FormComponent
